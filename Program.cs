5b68f533362ab1399f732b4702fcf2781712b0ef
nitroignika <commandagramhelp@gmail.com>
2016-09-16 21:34:44 -0400

    Added Base Bot Code

--- Program.cs
+++ Program.cs
@@ -14,6 +14,41 @@ namespace emily_bot_cs
     {
         static void Main(string[] args)
         {
+            new Program().Start();
         }
+
+        private DiscordClient _client;
+
+        public void Start()
+        {
+            _client = new DiscordClient(x =>
+            {
+                x.AppName = "Emily Bot";
+                x.AppUrl = "http://nitroignika.github.io";
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            _client.UsingCommands(x =>
+            {
+                x.PrefixChar = '~';
+                x.AllowMentionPrefix = true;
+            });
+
+            var token = "MjE4OTE5ODU5NDgyNzg3ODQw.Cr4uUQ.Fla_aKtnW7YDtP_QyXGsBxdGZWw";
+
+            _client.ExecuteAndWait(async () => 
+            {
+                await _client.Connect(token);
+            });
+        }
+
+
+
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine($"[{e.Severity}] [{e.Source}] [{e.Message}]");
+        }
+
     }
 }
