f75adc38981f7854db5aa4089191540b651eed1e
nitroignika <commandagramhelp@gmail.com>
2016-09-17 23:32:22 -0400

    Added Meme Module

---
+++ Modules/Memes/memes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Modules;
+using Discord.Commands;
+using Discord.Commands.Permissions.Levels;
+using emily.Enums;
+
+namespace emily.Modules.Basic_Commands
+{
+    class memes : IModule
+    {
+        private DiscordClient _client;
+        private ModuleManager _manager;
+
+        void IModule.Install(ModuleManager manager)
+        {
+            _manager = manager;
+            _client = manager.Client;
+
+            manager.CreateCommands("", cgb =>
+            {
+                cgb.MinPermissions((int)PermissionLevel.User);
+                #region ~meme
+                #endregion
+
+            });
+        }
+    }
+}
