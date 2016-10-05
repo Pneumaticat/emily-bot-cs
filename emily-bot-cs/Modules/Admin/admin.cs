35cf01efa3532616d009fb025645e5c8be90372d
Nikki <commandagramhelp@gmail.com>
2016-10-04 21:32:54 -0400

    Added ~deafen and ~undeafen commands

--- emily-bot-cs/Modules/Admin/admin.cs
+++ emily-bot-cs/Modules/Admin/admin.cs
@@ -149,6 +149,68 @@ namespace emily.Modules.admin
                     });
                 #endregion
 
+                #region ~deafen
+                cgb.CreateCommand("deafen")
+                    .Description("Deafen requested user")
+                    .Parameter("user")
+                    .MinPermissions((int)PermissionLevel.ServerAdmin)
+                    .Do(async e =>
+                    {
+                        ulong id;
+                        User m = null;
+                        string mentionedUser = e.Args[0];
+                        if (!string.IsNullOrWhiteSpace(mentionedUser))
+                        {
+                            if (e.Message.MentionedUsers.Count() == 1)
+                                m = e.Message.MentionedUsers.FirstOrDefault();
+                            else if (e.Server.FindUsers(mentionedUser).Any())
+                                m = e.Server.FindUsers(mentionedUser).FirstOrDefault();
+                            else if (ulong.TryParse(mentionedUser, out id))
+                                m = e.Server.GetUser(id);
+                        }
+
+                        if (m == null)
+                        {
+                            await e.Channel.SendMessage($"The user `{mentionedUser}` was not found! ");
+                            return;
+                        }
+
+                        await e.Channel.SendMessage($":hear_no_evil: {mentionedUser}");
+                        await m.Edit(isDeafened: true);
+                    });
+                #endregion
+
+                #region ~undeafen
+                cgb.CreateCommand("undeafen")
+                    .Description("Undeafen requested user")
+                    .Parameter("user")
+                    .MinPermissions((int)PermissionLevel.ServerAdmin)
+                    .Do(async e =>
+                    {
+                        ulong id;
+                        User m = null;
+                        string mentionedUser = e.Args[0];
+                        if (!string.IsNullOrWhiteSpace(mentionedUser))
+                        {
+                            if (e.Message.MentionedUsers.Count() == 1)
+                                m = e.Message.MentionedUsers.FirstOrDefault();
+                            else if (e.Server.FindUsers(mentionedUser).Any())
+                                m = e.Server.FindUsers(mentionedUser).FirstOrDefault();
+                            else if (ulong.TryParse(mentionedUser, out id))
+                                m = e.Server.GetUser(id);
+                        }
+
+                        if (m == null)
+                        {
+                            await e.Channel.SendMessage($"The user `{mentionedUser}` was not found! ");
+                            return;
+                        }
+
+                        await e.Channel.SendMessage($":unlock: {mentionedUser}");
+                        await m.Edit(isDeafened: false);
+                    });
+                #endregion
+
                 #region ~prune
                 cgb.CreateCommand("Purge")
                 .MinPermissions((int)PermissionLevel.ServerAdmin)
